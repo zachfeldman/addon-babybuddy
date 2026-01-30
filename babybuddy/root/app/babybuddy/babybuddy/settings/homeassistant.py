38b40c39a5174eb598ee444a6e5ab42346357f8a
Cody <intecpsp@gmail.com>
2026-01-30 09:54:07 -0500

    fix: trust reverse proxy headers to resolve CSRF issues

--- babybuddy/root/app/babybuddy/babybuddy/settings/homeassistant.py
+++ babybuddy/root/app/babybuddy/babybuddy/settings/homeassistant.py
@@ -1,6 +1,11 @@
 from .base import *
 import os
 
+# Trust the headers passed by Ingress/Nginx
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+USE_X_FORWARDED_HOST = True
+USE_X_FORWARDED_PORT = True
+
 ENABLE_HOME_ASSISTANT_SUPPORT = True
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 

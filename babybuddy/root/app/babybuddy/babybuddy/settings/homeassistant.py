81886425dd307bbf54fe0c6d563523fda26a65ed
Cody <intecpsp@gmail.com>
2026-01-30 09:24:33 -0500

    fix: apply CSRF_TRUSTED_ORIGINS config to Django settings
    Closes #81

--- babybuddy/root/app/babybuddy/babybuddy/settings/homeassistant.py
+++ babybuddy/root/app/babybuddy/babybuddy/settings/homeassistant.py
@@ -1,4 +1,8 @@
 from .base import *
+import os
 
 ENABLE_HOME_ASSISTANT_SUPPORT = True
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
+
+if os.getenv("CSRF_TRUSTED_ORIGINS"):
+    CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS").split(",")
